76e7e61d763721208f39769762068fd0c1d0cc43
Simon Rice <im@simonrice.com>
2020-02-01 15:18:33 +0000

    Cache renamings

--- netlify_deploy.sh
+++ netlify_deploy.sh
@@ -7,15 +7,15 @@ export NETLIFY_CACHE_DIR="/opt/build/cache"
 mkdir -p $NETLIFY_CACHE_DIR/swift-custom
 
 # Restore SPM Cache
-if [ -d $NETLIFY_CACHE_DIR/swift-custom/.build ]
+if [ -d $NETLIFY_CACHE_DIR/swift-custom/swift-build ]
 then
   rm -rf .build
-  cp -p -r $NETLIFY_CACHE_DIR/swift-custom/.build .build
+  cp -p -r $NETLIFY_CACHE_DIR/swift-custom/swift-build .build
 fi
 
 eval "$(cat install_swift.sh)"
 swift run
 
 # Cache new build dir
-rm -rf $NETLIFY_CACHE_DIR/swift-custom/.build
-cp -p -r .build $NETLIFY_CACHE_DIR/swift-custom/.build
+rm -rf $NETLIFY_CACHE_DIR/swift-custom/swift-build
+cp -p -r .build $NETLIFY_CACHE_DIR/swift-custom/swift-build
