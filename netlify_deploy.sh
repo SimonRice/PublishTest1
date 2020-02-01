06ab20a44f5f48676595648dd0ce9cfcd351405a
Simon Rice <im@simonrice.com>
2020-02-01 11:57:17 +0000

    Failing build

--- netlify_deploy.sh
+++ netlify_deploy.sh
@@ -3,28 +3,29 @@
 unset URL
 
 # Create caches (if not there)
-export NETLIFY_CACHE_DIR="/opt/buildhome/cache"
-mkdir -p $NETLIFY_CACHE_DIR/swift-custom
+# export NETLIFY_CACHE_DIR="/opt/buildhome/cache"
+# mkdir -p $NETLIFY_CACHE_DIR/swift-custom
 
 echo "Showing Swift cache"
-ls -la $NETLIFY_CACHE_DIR/
-ls -la $NETLIFY_CACHE_DIR/swift-custom/
+echo $NETLIFY_CACHE_DIR
+# ls -la $NETLIFY_CACHE_DIR/
+# ls -la $NETLIFY_CACHE_DIR/swift-custom/
 
 # Restore SPM Cache
-if [ -d $NETLIFY_CACHE_DIR/swift-custom/.build ]
-then
-  rm -rf .build
-  cp -p -r $NETLIFY_CACHE_DIR/swift-custom/.build .
-fi
+# if [ -d $NETLIFY_CACHE_DIR/swift-custom/.build ]
+# then
+#   rm -rf .build
+#   cp -p -r $NETLIFY_CACHE_DIR/swift-custom/.build .
+# fi
 
 eval "$(cat install_swift.sh)"
 swift run
 
 # Cache new build dir
 
-rm -rf $NETLIFY_CACHE_DIR/swift-custom/.build
-cp -p -r .build $NETLIFY_CACHE_DIR/swift-custom/.build
+# rm -rf $NETLIFY_CACHE_DIR/swift-custom/.build
+# cp -p -r .build $NETLIFY_CACHE_DIR/swift-custom/.build
 
-ls -la $NETLIFY_CACHE_DIR/swift-custom/
-ls -la $NETLIFY_CACHE_DIR/swift-custom/.build
-ls -la $NETLIFY_CACHE_DIR/swift-custom/swift_version
+# ls -la $NETLIFY_CACHE_DIR/swift-custom/
+# ls -la $NETLIFY_CACHE_DIR/swift-custom/.build
+# ls -la $NETLIFY_CACHE_DIR/swift-custom/swift_version
