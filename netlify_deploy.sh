797f8652eaadf0509e41072ec11d602b9ed216e9
Simon Rice <im@simonrice.com>
2020-02-01 11:27:58 +0000

    le ls

--- netlify_deploy.sh
+++ netlify_deploy.sh
@@ -17,9 +17,10 @@ eval "$(cat install_swift.sh)"
 swift run
 
 # Cache new build dir
+
 rm -rf $NETLIFY_CACHE_DIR/swift-custom/.build
 cp -p -r .build $NETLIFY_CACHE_DIR/swift-custom/.
 
 ls -l $NETLIFY_CACHE_DIR/swift-custom/
 ls -l $NETLIFY_CACHE_DIR/swift-custom/.build
-le -l $NETLIFY_CACHE_DIR/swift-custom/swift_version
+ls -l $NETLIFY_CACHE_DIR/swift-custom/swift_version
